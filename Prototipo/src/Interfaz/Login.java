7a7f9ab637e2022af5eaab1b67fb00a754cf5a20
Jefry99XD <75385895+Jefry99XD@users.noreply.github.com>
2022-09-19 23:12:50 -0600

    con 3 comparaciones

--- Prototipo/src/Interfaz/Login.java
+++ Prototipo/src/Interfaz/Login.java
@@ -11,6 +11,7 @@ import Logic.ControllerAuth;
 import Logic.merge;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.ArrayList;
 /**
  *
  * @author Aldokler
@@ -20,20 +21,33 @@ public class Login {
     /**
      * @param args the command line arguments
      */
+    public static String getUsuario(String[] usuarios, String usuarioBuscar){
+        String usuario = "";
+        for(int i =0;i<usuarios.length;i++){
+            if(usuarios[i] ==usuarioBuscar){
+                usuario = usuarios[i];
+                break;
+            }
+        }
+        return usuario;
+    }
     public static void main(String[] args) {
         Date t = new Date();
         Long X =t.getTime();
         LoginEmail window = new LoginEmail();
         funcionesUsuarios f = new funcionesUsuarios();
-        String[] users = f.getUsers();
+        //String[] users = f.getUsers();
         //for (String user : users) {
           //  System.out.println(user);
         //}
-        merge.mergeSort(users, 0, users.length-1);
+        
+        //merge.mergeSort(users, 0, users.length-1);
         //for (String user : users) {
           //  System.out.println(user);
         //}
-        System.out.println(Arrays.binarySearch(users, "usuario-65"));
+        System.out.println(f.findUser("usuario-56642"));
+        //System.out.println(getUsuario(users, "usuario-1"));
+        //System.out.println(Arrays.binarySearch(users, "usuario-65"));
         Date t2 = new Date();
         Long Y =t2.getTime();
         System.out.println((Y-X));
