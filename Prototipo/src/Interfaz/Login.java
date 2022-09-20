b0f3d45266e75b0a157527363ef30ea000030251
Jefry99XD <75385895+Jefry99XD@users.noreply.github.com>
2022-09-19 22:55:10 -0600

    Con el date para ver los segundos

--- Prototipo/src/Interfaz/Login.java
+++ Prototipo/src/Interfaz/Login.java
@@ -9,7 +9,8 @@ import BD.User;
 import BD.funcionesUsuarios;
 import Logic.ControllerAuth;
 import Logic.merge;
-
+import java.util.Arrays;
+import java.util.Date;
 /**
  *
  * @author Aldokler
@@ -20,16 +21,22 @@ public class Login {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Date t = new Date();
+        Long X =t.getTime();
         LoginEmail window = new LoginEmail();
         funcionesUsuarios f = new funcionesUsuarios();
         String[] users = f.getUsers();
-        for (String user : users) {
-            System.out.println(user);
-        }
+        //for (String user : users) {
+          //  System.out.println(user);
+        //}
         merge.mergeSort(users, 0, users.length-1);
-        for (String user : users) {
-            System.out.println(user);
-        }
+        //for (String user : users) {
+          //  System.out.println(user);
+        //}
+        System.out.println(Arrays.binarySearch(users, "usuario-65"));
+        Date t2 = new Date();
+        Long Y =t2.getTime();
+        System.out.println((Y-X));
         window.setVisible(true);
         window.setLocationRelativeTo(null);         
     }
